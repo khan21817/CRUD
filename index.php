4b910f0ead28704baa1070c2f8fb78946613720b
khan21817 <sajibk244@gmail.com>
2020-12-23 11:48:59 +0100

    delete function

--- index.php
+++ index.php
@@ -3,34 +3,46 @@ require_once 'inc/functions.php';
 $info = '';
 $task = $_GET['task'] ?? 'report';
 
+if ('delete' == $task) {
+    $id = filter_input (INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+    if ($id > 0) {
+        deleteStudent ($id);
+        header ('location:/index.php?task=report');
 
-if ( 'seed' == $task ) {
-    seed();
+    }
+
+}
+
+if ('seed' == $task) {
+    seed ();
     $info = "Seeding is complete";
 }
 
-if (isset($_POST['submit'])){
-    $fname = filter_input (INPUT_POST, 'fname',FILTER_SANITIZE_STRING);
-    $lname = filter_input (INPUT_POST, 'lname',FILTER_SANITIZE_STRING);
-    $roll = filter_input (INPUT_POST, 'roll',FILTER_SANITIZE_STRING);
-    $id = filter_input (INPUT_POST, 'id',FILTER_SANITIZE_STRING);
+if (isset($_POST['submit'])) {
+    $fname = filter_input (INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
+    $lname = filter_input (INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
+    $roll = filter_input (INPUT_POST, 'roll', FILTER_SANITIZE_STRING);
+    $id = filter_input (INPUT_POST, 'id', FILTER_SANITIZE_STRING);
 
-    if ($id){
-        if ($fname!='' && $lname!='' && $roll!=''){
-          $result =   updateStudent($id,$fname,$lname,$roll);
-          if ($result){
-              header ('location:/index.php?task=report');
-          }else{
-              header ('location:/error.php');
-          }
-           
-        }
-    }else{
-        if ($fname!='' && $lname!='' && $roll!=''){
-            $result = addStudents($fname,$lname,$roll);
-            if ($result){
+    if ($id) {
+        if ($fname != '' && $lname != '' && $roll != '') {
+            $result = updateStudent ($id, $fname, $lname, $roll);
+            if ($result) {
                 header ('location:/index.php?task=report');
-            }else{
+            }
+            else {
+                header ('location:/error.php');
+            }
+
+        }
+    }
+    else {
+        if ($fname != '' && $lname != '' && $roll != '') {
+            $result = addStudents ($fname, $lname, $roll);
+            if ($result) {
+                header ('location:/index.php?task=report');
+            }
+            else {
                 header ('location:/error.php');
             }
 
@@ -61,23 +73,28 @@ if (isset($_POST['submit'])){
         <div class="column column-60 column-offset-20">
             <h2>Project 2 - CRUD</h2>
             <p>A sample project to perform CRUD operations using plain files and PHP</p>
-            <?php include_once 'inc/templates/nav.php' ; ?>
+            <?php
+            include_once 'inc/templates/nav.php'; ?>
             <hr/>
             <?php
-            if ( $info != '' ) {
+            if ($info != '') {
                 echo "<p>{$info}</p>";
             }
             ?>
         </div>
     </div>
-    <?php if ( 'report' == $task ): ?>
+    <?php
+    if ('report' == $task): ?>
         <div class="row">
             <div class="column column-60 column-offset-20">
-                <?php  generateReport(); ?>
+                <?php
+                generateReport (); ?>
             </div>
         </div>
-    <?php endif; ?>
-    <?php if ( 'add' == $task ): ?>
+    <?php
+    endif; ?>
+    <?php
+    if ('add' == $task): ?>
         <div class="row">
             <div class="column column-60 column-offset-20">
                 <form action="/index.php?report" method="post">
@@ -87,32 +104,39 @@ if (isset($_POST['submit'])){
                     <input type="text" name="lname" id="lname">
                     <label for="roll">Roll</label>
                     <input type="number" name="roll" id="roll">
-                    <button type="submit" class="button-primary" name="submit" >Save</button>
+                    <button type="submit" class="button-primary" name="submit">Save</button>
                 </form>
             </div>
         </div>
-    <?php endif; ?>
-    <?php if ( 'edit' == $task ):
+    <?php
+    endif; ?>
+    <?php
+    if ('edit' == $task):
 
-        $id = filter_input (INPUT_GET, 'id',FILTER_SANITIZE_STRING);
-        $student = getStudent($id);
+        $id = filter_input (INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+        $student = getStudent ($id);
         if ($student):
-        ?>
-        <div class="row">
-            <div class="column column-60 column-offset-20">
-                <form action="/index.php?task=edit" method="post">
-                    <input type="hidden" value="<?php echo $id?>" name="id">
-                    <label for="fname">First Name</label>
-                    <input type="text" name="fname" id="fname" value="<?php echo $student['fname'];?>">
-                    <label for="lname">Last Name</label>
-                    <input type="text" name="lname" id="lname" value="<?php echo $student['lname'];?>">
-                    <label for="roll">Roll</label>
-                    <input type="number" name="roll" id="roll" value="<?php echo $student['roll'];?>">
-                    <button type="submit" class="button-primary" name="submit" >update</button>
-                </form>
+            ?>
+            <div class="row">
+                <div class="column column-60 column-offset-20">
+                    <form action="/index.php?task=edit" method="post">
+                        <input type="hidden" value="<?php
+                        echo $id ?>" name="id">
+                        <label for="fname">First Name</label>
+                        <input type="text" name="fname" id="fname" value="<?php
+                        echo $student['fname']; ?>">
+                        <label for="lname">Last Name</label>
+                        <input type="text" name="lname" id="lname" value="<?php
+                        echo $student['lname']; ?>">
+                        <label for="roll">Roll</label>
+                        <input type="number" name="roll" id="roll" value="<?php
+                        echo $student['roll']; ?>">
+                        <button type="submit" class="button-primary" name="submit">update</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    <?php endif;
+        <?php
+        endif;
     endif; ?>
 </div>
 </body>
